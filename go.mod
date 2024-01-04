f673a8d7ccc8b453aeb18303ff4cbe7351b4c1bc
Bin Zhang <zncoder@gmail.com>
2024-01-03 20:55:34 -0800

    ParseFlag

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zncoder/mygo
+
+go 1.21.0
+
+require github.com/zncoder/check v0.0.0-20240103200842-893be3e81944
---
+++ mygo.go
@@ -0,0 +1,33 @@
+package mygo
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/zncoder/check"
+)
+
+// ParseFlag specifies non-flag args.
+// Ex. ParseFlag("required_arg0", "[optional_arg1]")
+// Optional args must appear after required args.
+func ParseFlag(args ...string) {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s %s\n", os.Args[0], strings.Join(args, " "))
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	var opt bool
+	n := 0
+	for _, arg := range args {
+		if !strings.HasPrefix(arg, "[") {
+			n++
+			check.T(!opt).F("required arg appears after optional args", "args", args)
+		} else {
+			opt = true
+		}
+	}
+	check.T(n <= flag.NArg()).F("not enough required args", "args", args, "flag_args", flag.Args())
+}
